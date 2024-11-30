7c8474c43de849639d7bcd2084497ab67ee111ed
annapirz <fanworld991@gmail.com>
2024-11-30 15:33:04 +0300

    fix: form select(option)

--- src/companeents/Form/Form.jsx
+++ src/companeents/Form/Form.jsx
@@ -41,7 +41,7 @@ const Form = () => {
             <input className={'input'} type={'text'} placeholder={'улица'} value={street} onChange={onChangeStreet} />
             <select className={'select'} value={subject} onChange={onChangeSubject}>
                 <option value={'legal'}>Юр. лицо</option>
-                <option value={'legal'}>Физ. лицо</option>
+                <option value={'physical'}>Физ. лицо</option>
             </select>
         </div>
     );
